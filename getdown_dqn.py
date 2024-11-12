ab08ff79dc8c57d1698b2a8fc047ca024aab0c4b
ChenRui <370299412@qq.com>
2024-11-12 21:45:43 +0800

    11/12: 修改奖励分值

--- getdown_dqn.py
+++ getdown_dqn.py
@@ -8,9 +8,11 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from matplotlib import pyplot as plt
-
+import logging
 from getdown import Hell, SCREEN_WIDTH, SCREEN_HEIGHT, DEADLY
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class DQNAgent(nn.Module):
     def __init__(self, state_size, action_size):
@@ -70,9 +72,9 @@ class DQNAgent(nn.Module):
     def load_model(self, file_name):
         if os.path.exists(file_name):
             self.model.load_state_dict(torch.load(file_name))
-            print(f"Model loaded from {file_name}")
+            logger.info(f"Model loaded from {file_name}")
         else:
-            print(f"No model found at {file_name}, starting from scratch.")
+            logger.info(f"No model found at {file_name}, starting from scratch.")
 
 
 # 环境类的示例
@@ -117,9 +119,13 @@ class Env:
 
         # 判断物体所处的位置控制在100~400之间
         if 100 < body.y < 400:
-            reward += 1
+            reward += 6
+        elif 150 < body.y < 350:
+            reward += 8
+        elif 200 < body.y < 300:
+            reward += 10
         else:
-            reward -= 1
+            reward -= 5
 
         # 当物体成功离开本级台阶或达到下面某级台阶时，可以提供额外奖励。
         if matching_barriers:
@@ -127,9 +133,9 @@ class Env:
             right_distance = matching_barriers[0].rect.x + matching_barriers[0].rect.width - body.x - body.h
             # 说明在台面上移动
             if left_distance < right_distance and action == 0:
-                reward += 1
+                reward += 4
             elif left_distance > right_distance and action == 1:
-                reward += 1
+                reward += 4
             else:
                 reward -= 2
 
@@ -140,16 +146,14 @@ class Env:
 
         # 对于不良行为，给予负奖励。例如，下方快碰到带刺的障碍时
         if matching_barriers and matching_barriers[0].type == DEADLY:
-            reward -= 5
-        else:
-            reward += 3
+            reward -= 7
 
         # 当物体到达新的区域或发现新的障碍物时，可以给予奖励。
         if self.preview_barrier_num < len(self.hell.barrier):
             self.preview_barrier_num = len(self.hell.barrier)
-            reward += 1
+            reward += 2
         else:
-            reward -= 0.5
+            reward -= 1
 
         # 增加下落时朝向障碍物的奖励
         falling_towards_barrier = any(
@@ -157,10 +161,10 @@ class Env:
             for ba in barrier
         )
         if falling_towards_barrier:
-            reward += 1
+            reward += 3
 
         # 为每个时间步骤提供小的正奖励，以鼓励持续进行游戏。
-        reward += 2
+        reward += 5
         return reward
 
     def get_state(self):
@@ -197,7 +201,11 @@ if __name__ == "__main__":
     total_game_num = 0
     rewards = []  # 用于记录每个回合的总奖励
 
+    # 记录之前最好的成绩
+    best_score = 0
+
     try:
+
         state = env.reset()
         while True:  # 无限训练
 
@@ -220,10 +228,20 @@ if __name__ == "__main__":
                 rewards.pop(0)
 
             if done:
-                print(f"Total game num: {total_game_num},Total Steps: {total_steps}, total score: {env.hell.score}, Epsilon: {agent.epsilon:.7}")
-                print(f'current step:{total_steps}, save getdown_hell_model.h5')
+                # 获取当前回合的得分
+                current_score = env.hell.score
+                logger.info(
+                    f"Total game num: {total_game_num}, Total Steps: {total_steps}, Total score: {current_score}, Epsilon: {agent.epsilon:.7f}")
+
+                # 判断当前得分是否超过阈值
+                if current_score >= best_score:
+                    logger.info(f'Saving model to getdown_hell_model.h5')
+                    agent.save_model("getdown_hell_model.h5")
+                    best_score = current_score
+                else:
+                    logger.info('Model not saved: performance did not meet threshold.')
+
                 total_steps = 0
-                agent.save_model("getdown_hell_model.h5")  # 保存模型
                 total_game_num += 1
                 env.hell.reset()
 
@@ -231,7 +249,7 @@ if __name__ == "__main__":
                 agent.replay(32)
 
     except KeyboardInterrupt:
-        print("\nTraining interrupted. Saving model...")
+        logger.info("\nTraining interrupted. Saving model...")
         agent.save_model("getdown_hell_model.h5")  # 保存模型
 
     # 绘制奖励线图
